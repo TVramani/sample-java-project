8c948601f978e4add446dda46e20733248d9455d
TLramani <ramanivh@gmail.com>
2020-02-19 20:38:55 -0700

    initial checkin

---
+++ src/main/java/com/my/examples/App.java
@@ -0,0 +1,15 @@
+package com.mkyong.hashing;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class App {
+
+    public static void main(String[] args) {
+
+			System.out.println("Hello Ramani !!");
+
+    }
+
+   
+
+}
